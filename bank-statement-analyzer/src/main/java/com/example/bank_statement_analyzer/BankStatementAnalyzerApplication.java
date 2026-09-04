bf2dbf3877bc73a66e20a299ab22305a0df03f6d
Ilyas Siddiqui <siddiqui.ilyas1112@gmail.com>
2026-09-04 23:54:21 +0530

    Initial Spring Boot project setup

---
+++ bank-statement-analyzer/src/main/java/com/example/bank_statement_analyzer/BankStatementAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bank_statement_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankStatementAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankStatementAnalyzerApplication.class, args);
+	}
+
+}
